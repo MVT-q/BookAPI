071f42cf605a45be59402206c52ca4192b7ce3d4
Yehor Radykop <egorradikop@gmail.com>
2026-07-22 14:31:36 +0200

    Implement JWT token generation

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,4 +1,8 @@
 ﻿using BookApi.DTOs;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BookApi.Services
 {
@@ -13,16 +17,38 @@ namespace BookApi.Services
 
         public LoginResponseDto? Login(LoginDto request)
         {
-            if (request.Username == "admin" && request.Password == "123456")
+            if (request.Username != "admin" || request.Password != "123456")
             {
-                return new LoginResponseDto
-                {
-                    Token = "temporary-token"
-                };
+                return null;
             }
 
-            else
-                return null;
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, request.Username)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+
+            var credentials = new SigningCredentials(
+                key: key,
+                algorithm: SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(30),
+                signingCredentials: credentials);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            
+            string tokenString = tokenHandler.WriteToken(token);
+
+            return new LoginResponseDto 
+            {
+                Token = tokenString,
+            };
         }
     }
 }
